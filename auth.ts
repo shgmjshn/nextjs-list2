f8d55c6df59eddd393ac6c68fede6a8a97877a71
root <root@kyokiscientist>
2025-02-12 14:47:49 +0900

    チュートリアル完走

--- auth.ts
+++ auth.ts
@@ -32,12 +32,12 @@ export const { auth, signIn, signOut } = NextAuth({
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
 
-            if (passwordsMatch) return user;
-        }
+              if (passwordsMatch) return user;
+          }
 
-        console.log('Invalid credentials');
-        return null;
-    },
-  }),
-],
+          console.log('Invalid credentials');
+          return null;
+      },
+    }),
+  ],
 });
